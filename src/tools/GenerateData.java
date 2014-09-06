fd1e01a5c0b5fe1cc582f6184057ba7ac8f11465
haden dude <haden.dude@gmail.com>
2014-09-06 07:11:33 -0700

    improving data generator

--- src/tools/GenerateData.java
+++ src/tools/GenerateData.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Calendar;
 import java.util.Random;
-import java.util.TimeZone;
 import java.util.zip.GZIPOutputStream;
 
 import org.slf4j.Logger;
@@ -16,85 +15,105 @@ import org.slf4j.LoggerFactory;
 public class GenerateData {
 	private static final Logger LOG = LoggerFactory.getLogger(GenerateData.class);
 
-//	private static final int RANDOM_RANGE = 101;
-//	private static final int RANDOM_GAP = 50;
+	//	private static final int RANDOM_RANGE = 101;
+	//	private static final int RANDOM_GAP = 50;
 
 
 	/** Prints usage and exits.  */
 	static void usage(final ArgP argp) {
-	  System.err.println("Usage: generate --metric=NAME --year=YYY --num=NUM_YEARS [--compress] [--millis]");
-	  System.err.print(argp.usage());
-	  System.exit(-1);
+		System.err.println("Usage: generate --metric=NAME --year=YYY");
+		System.err.print(argp.usage());
+		System.exit(-1);
 	}
 
 	public static void main(String[] args) throws IOException {
-	    ArgP argp = new ArgP();
-	    argp.addOption("--metric", "NAME", "Output file name basis");
-	    argp.addOption("--year", "YYYY", "starting year");
-	    argp.addOption("--num", "NUM_YEARS", "number of years generated");
-	    argp.addOption("--compress", "[optional] compress generated file");
-	    argp.addOption("--millis", "[optional] use milliseconds");
-	    
-	    if (args.length < 3) {
-	    	usage(argp);
-	    }
-	    
-	    args = CliOptions.parse(argp, args);
-	    
-	    boolean useCompression = argp.has("--compress");
-	    boolean useMillis = argp.has("--millis");
-	    String metricName = argp.get("--metric");
-	    int startYear = Integer.parseInt(argp.get("--year"));
-	    int totalYears = Integer.parseInt(argp.get("--num"));
+		ArgP argp = new ArgP();
+		argp.addOption("--metric", "NAME", "Output file name basis");
+		argp.addOption("--num-metrics", "METRICS", "(default 1) number of metrics generated");
+		argp.addOption("--num-tagk", "NUM_TAGK", "(default 1) number of tags for each data point");
+		argp.addOption("--num-tagv", "NUM_TAGV", "(default 1) number of different values generated for each tag");
+		argp.addOption("--year", "YYYY", "starting year");
+		argp.addOption("--num-years", "YEARS", "(default 1) number of years generated");
+		argp.addOption("--num-weeks", "WEEKS",  "(default 52) total number of weeks to generate per year");
+		argp.addOption("--pph", "SAMPLE_SIZE", "(default 3600) number of data points per hour");
+		argp.addOption("--range", "RANGE", "(default 101) value += random(range) - gap");
+		argp.addOption("--gap", "GAP", "(default 50) value += random(range) - gap");
+		argp.addOption("--compress", "[optional] compress generated file");
+		if (args.length < 3) {
+			usage(argp);
+		}
 
-	    LOG.info("Generating {} years starting from year {}, for metric {}.", totalYears, startYear, metricName);
-	    if (useMillis) {
-	    	LOG.info("Using MilliSeconds");
-	    }
-	    if (useCompression) {
-	    	LOG.info("Files will be compressed");
-	    }
-	    
-		generateYearlyFiles(useCompression, useMillis, metricName, startYear, totalYears, new Random());
+		args = CliOptions.parse(argp, args);
+
+		String metricName = argp.get("--metric");
+		int numMetrics = Integer.parseInt(argp.get("--num-metrics", "1"));
+		int numTagK = Integer.parseInt(argp.get("--num-tagk", "1"));
+		int numTagV = Integer.parseInt(argp.get("--num-tagv", "1"));
+		int startYear = Integer.parseInt(argp.get("--year"));
+		int totalYears = Integer.parseInt(argp.get("--num-years", "1"));
+		int weeks = Integer.parseInt(argp.get("--num-weeks", "52"));
+		boolean useCompression = argp.has("--compress");
+		int pph = Integer.parseInt(argp.get("--pph", "3600"));
+		int range = Integer.parseInt(argp.get("--range", "101"));
+		int gap = Integer.parseInt(argp.get("--gap", "50"));
+
+		if (pph <= 0) {
+			System.err.println("pph must be a positive number");
+			System.exit(-1);
+		}
+
+		LOG.info("Generating {} years starting from year {}, for metric {} with {} points per hour", totalYears, startYear, metricName, pph);
+		LOG.info("Generating {} metrics with {} tags and {} values for each tag", numMetrics, numTagK, numTagV);
+
+		generateYearlyFiles(useCompression, metricName, numMetrics, numTagK, numTagV, startYear, totalYears, weeks, pph, range, gap, new Random());
 	}
-	
-	public static void generateYearlyFiles(boolean useCompression, boolean useMillis, String metricName, final int startYear, final int totalYears, final Random rand) throws IOException {
+
+	public static void generateYearlyFiles(final boolean useCompression, final String metricName, final int numMetrics, final int numTagK, final int numTagV, final int startYear, final int totalYears, final int weeks, final int pph, final int range, final int gap, final Random rand) throws IOException {
 		//TODO add timezone (--tz) param to argp
 		Calendar cal = Calendar.getInstance(); // use local timezone
 		cal.set(startYear, 0, 1, 0, 0, 0);
 
 		String extension = useCompression ? ".tsd.gz" : ".tsd";
-		int sampleSize = useMillis ? 3600000 : 3600;
-		long value = 1; // rand.nextInt(RANDOM_RANGE) - RANDOM_GAP;
+
+		long value = rand.nextInt(range) - gap;
 		long count = 0;
+		
+		int[] tagValues = new int[numTagK]; 
 
 		long startTime = System.currentTimeMillis();
 
 		for (int year = startYear; year < startYear + totalYears; year++) {
-			LOG.info("generating year {}", year);
+			LOG.info("generating year {} / {}", year, totalYears);
 
-			File metricFolder = new File(""+year);
-			metricFolder.mkdirs();
-			
-			File metricFile = new File(metricFolder, metricName + extension);
+			File metricFile = new File("" + year + "-" + metricName + extension);
 			OutputStream os = createOutputStream(useCompression, metricFile);
 
-			long time = useMillis ? cal.getTimeInMillis() : cal.getTimeInMillis() / 1000;
-			for (int week = 1; week <= 52; week++) {
-				LOG.info("Week {} / 52", week);
+			long time = (pph > 3600) ? cal.getTimeInMillis() : cal.getTimeInMillis() / 1000;
+			int time_inc = (pph > 3600) ? 3600000 / pph : 3600 / pph;
+			
+			for (int week = 1; week <= weeks; week++) {
+				LOG.info("Week {} / {}", week, weeks);
 				for (int day = 0; day < 7; day++) {
 					for (int hour = 0; hour < 24; hour++) {
-						for (int i = 0; i < sampleSize; i++) {
-							writeRecord(os, metricName, week, day, hour, time, value);
+						for (int i = 0; i < pph; i++) {
+							
+							for (int v = 0; v < numTagK; v++) {
+								tagValues[v] = rand.nextInt(numTagV);
+							}
+							
+							writeRecord(os, metricName + "." + rand.nextInt(numMetrics), time, value, tagValues);
+							
 							// Alter the value by a range of +/- RANDOM_GAP
-							// value += rand.nextInt(RANDOM_RANGE) - RANDOM_GAP;
-							time++;
+							value += rand.nextInt(range) - gap;
+							
+							time+= time_inc;
+							
 							count++;
 						}
 					}
 				}
 			}
-			
+
 			os.flush();
 			os.close();
 
@@ -104,17 +123,33 @@ public class GenerateData {
 		//TODO display total number of data points
 		LOG.info("Total time to create {} data points: {}ms", count, totalTime);
 	}
-	
+
 	private static OutputStream createOutputStream(boolean useCompression, File path) throws IOException {
 		FileOutputStream fos = new FileOutputStream(path);
 		return useCompression
 				? new BufferedOutputStream(new GZIPOutputStream(fos))
-				: new BufferedOutputStream(fos);
+		: new BufferedOutputStream(fos);
 	}
-	
-	private static void writeRecord(OutputStream os, String metricName, int week, int day, int hour, long time, long value) throws IOException {
-		String record = metricName + " " + time + " " + value + " " + 
-				"week=" + week + " " + "day=" + day /*+ " "+ "hour=" + hour*/ +"\n";
-		os.write(record.getBytes());
+
+	private static void writeRecord(OutputStream os, String metricName, long time, long value, int[] tagValues) throws IOException {
+		StringBuffer record = new StringBuffer();
+		record.append(metricName)
+		.append(" ")
+		.append(time)
+		.append(" ")
+		.append(value);
+		
+		for (int v = 0; v < tagValues.length; v++) {
+			record.append(" ")
+			.append("tag")
+			.append(v)
+			.append("=")
+			.append("value")
+			.append(tagValues[v]);
+		}
+		
+		record.append("\n");
+		
+		os.write(record.toString().getBytes());
 	}
 }
