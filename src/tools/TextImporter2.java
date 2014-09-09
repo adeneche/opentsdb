cf80cc4981671a527c4928f1d85c1ecd898bfbc8
haden dude <haden.dude@gmail.com>
2014-09-09 06:18:57 -0700

    adding parameter for repeat and duplicate features

--- src/tools/TextImporter2.java
+++ src/tools/TextImporter2.java
@@ -17,15 +17,14 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.zip.GZIPInputStream;
 
 import net.opentsdb.core.CachedBatches;
-import net.opentsdb.core.Const;
 import net.opentsdb.core.TSDB;
 import net.opentsdb.core.Tags;
 import net.opentsdb.utils.Config;
-import net.opentsdb.utils.DateTime;
 
 import org.hbase.async.HBaseClient;
 import org.slf4j.Logger;
@@ -33,166 +32,185 @@ import org.slf4j.LoggerFactory;
 
 final class TextImporter2 {
 
-  private static final Logger LOG = LoggerFactory.getLogger(TextImporter2.class);
+	private static final Logger LOG = LoggerFactory.getLogger(TextImporter2.class);
 
-  /** Prints usage and exits with the given retval.  */
-  static void usage(final ArgP argp, final int retval) {
-    System.err.println("Usage: import path [more paths]");
-    System.err.print(argp.usage());
-    System.err.println("This tool can directly read gzip'ed input files.");
-    System.exit(retval);
-  }
-  
-  static long parseDuration(String arg) {
-	if (arg == null) return 0;
+	private static int numRepeats = 1;
+	private static int numDuplicates = 1;
+	private static String duplicateTag = "";
 	
-    if (arg.charAt(0) == '-') {
-      return -DateTime.parseDuration(arg.substring(1, arg.length()));
-	} else {
-	  return DateTime.parseDuration(arg);
+	/** Prints usage and exits.  */
+	private static void usage(final ArgP argp) {
+		System.err.println("Usage: import2 [repeat NUM-REPEATS] [duplicate NUM-DUPLICATES DUPLICATE-TAG-NAME] path [more paths]");
+		System.err.print(argp.usage());
+		System.err.println("This tool can directly read gzip'ed input files.");
+		System.exit(-1);
 	}
-  }
-  
-  static int parseRepeatCount(String arg) {
-	  if (arg == null) return 1; // this is an optional parameter, so it may be missing
-	  
-	  //TODO throw exception if count <= 0
-	  return Integer.parseInt(arg);
-  }
-
-  public static void main(String[] args) throws Exception {
-    ArgP argp = new ArgP();
-    CliOptions.addCommon(argp);
-    CliOptions.addAutoMetricFlag(argp);
-    argp.addOption("--offset", "time offset");
-    argp.addOption("--repeat_count", "number of times loaded data is repeated");
-    argp.addOption("--repeat_offset", "time offset each time a file is repeated");
-    args = CliOptions.parse(argp, args);
-    if (args == null) {
-      usage(argp, 1);
-    } else if (args.length < 1) { //TODO change this to account for our own parameters
-      usage(argp, 2);
-    }
-    
-    // get a config object
-    Config config = CliOptions.getConfig(argp);
-    
-    final long offset = parseDuration(argp.get("--offset"));
-    final int repeatCount = parseRepeatCount(argp.get("--repeat_count", "1"));
-    final long repeatOffset = repeatCount > 1 ? parseDuration(argp.get("--repeat_offset")) : 0;
-    //TODO exception if repeatCount > 1 AND repeatOffset == 0
-    
-    LOG.info("time_offset: "+offset);
-    LOG.info("repeat_count: "+repeatCount);
-    LOG.info("repeat_offset: "+repeatOffset);
-    
-    final TSDB tsdb = new TSDB(config);
-    tsdb.checkNecessaryTablesExist().joinUninterruptibly();
-    argp = null;
-    try {
-      int points = 0;
-      final long start_time = System.nanoTime();
-      
-      for (int repeat = 0; repeat < repeatCount; repeat++) {
-          for (final String path : args) {
-              points += importFile(tsdb.getClient(), tsdb, path, offset + repeat * repeatOffset);
-            }
-      }
-      
-      final double time_delta = (System.nanoTime() - start_time) / 1000000000.0;
-      LOG.info(String.format("Total: imported %d data points in %.3fs"
-                             + " (%.1f points/s)",
-                             points, time_delta, (points / time_delta)));
-    } finally {
-      try {
-        tsdb.shutdown().joinUninterruptibly();
-      } catch (Exception e) {
-        LOG.error("Unexpected exception", e);
-        System.exit(1);
-      }
-    }
-  }
-
-  private static int importFile(final HBaseClient client,
-                                final TSDB tsdb,
-                                final String path,
-                                final long offset) throws IOException {
-	LOG.info("importing file with offset: "+offset);
 	
-    final long start_time = System.nanoTime();
-    long ping_start_time = start_time;
-    final BufferedReader in = open(path);
-    String line = null;
-    int points = 0;
-    try {
-      while ((line = in.readLine()) != null) {
-        final String[] words = Tags.splitString(line, ' ');
-        final String metric = words[0];
-        if (metric.length() <= 0) {
-          throw new RuntimeException("invalid metric: " + metric);
-        }
-        long timestamp = Tags.parseLong(words[1]);
-        if (timestamp <= 0) {
-          throw new RuntimeException("invalid timestamp: " + timestamp);
-        }
+	private static String[] parseParams(String[] args) {
+		int cur = 0;
+		
+			// check if first param is "repeat num_repeats"
+			if ("repeat".equals(args[0])) {
+				if (args.length < 3) {
+					throw new RuntimeException("Not enough arguments");
+				}
+				
+				numRepeats = Integer.parseInt(args[1]);
+				if (numRepeats <= 0) {
+					throw new IllegalArgumentException("NUM-REPEATS must be greater than 0");
+				}
+				
+				cur += 2;
+			}
+			
+			// check if next param is "duplicate num_duplicates duplicate_tag_name"
+			if ("duplicate".equals(args[cur])) {
+				if (args.length - cur < 4) {
+					throw new RuntimeException("Not enough arguments");
+				}
+				
+				numDuplicates = Integer.parseInt(args[cur+1]);
+				if (numDuplicates <= 0) {
+					throw new IllegalArgumentException("NUM-DUPLICATES must be greater than 0");
+				}
+				
+				duplicateTag = args[cur+2];
+				Tags.validateString("DUPLICATE-TAG-NAME", duplicateTag);
+				
+				cur += 3;
+			}
+			
+			// return unparsed arguments
+			String[] unparsed = new String[args.length - cur];
+			for (int i = 0; cur < args.length; cur++, i++) {
+				unparsed[i] = args[cur];
+			}
+			
+			return unparsed;
+	}
 
-        if ((timestamp & Const.SECOND_MASK) == 0) { // time is in seconds
-        	timestamp *= 1000; // convert to ms
-        }
+	public static void main(String[] args) throws Exception {
+		ArgP argp = new ArgP();
+		CliOptions.addCommon(argp);
+		CliOptions.addAutoMetricFlag(argp);
 
-        timestamp += offset;
-        
-        final String value = words[2];
-        if (value.length() <= 0) {
-          throw new RuntimeException("invalid value: " + value);
-        }
-        final HashMap<String, String> tags = new HashMap<String, String>();
-        for (int i = 3; i < words.length; i++) {
-          if (!words[i].isEmpty()) {
-            Tags.parse(tags, words[i]);
-          }
-        }
+		args = CliOptions.parse(argp, args);
+		if (args == null || args.length < 1) {
+			usage(argp);
+		}
+		
+		try {
+			args = parseParams(args);
+		} catch (Exception e) {
+			usage(argp);
+		}
+		
+		LOG.info("repeat num: {}", numRepeats);
+		LOG.info("duplicates num: {} and tag {}", numDuplicates, duplicateTag);
+		LOG.info("paths: {}", Arrays.toString(args));
+		
+		System.exit(1);
 
-        CachedBatches.addPoint(tsdb, metric, timestamp, value, tags);
-        points++;
-        if (points % 1000000 == 0) {
-          final long now = System.nanoTime();
-          ping_start_time = (now - ping_start_time) / 1000000;
-          LOG.info(String.format("... %d data points in %dms (%.1f points/s), freemem: %d",
-                                 points, ping_start_time,
-                                 (1000000 * 1000.0 / ping_start_time), 
-                                 Runtime.getRuntime().freeMemory()));
-          ping_start_time = now;
-        }
-      }
-    } catch (RuntimeException e) {
-        LOG.error("Exception caught while processing file "
-                  + path + " line=" + line);
-        throw e;
-    } finally {
-      in.close();
-    }
-    final long time_delta = (System.nanoTime() - start_time) / 1000000;
-    LOG.info(String.format("Processed %s in %d ms, %d data points"
-                           + " (%.1f points/s)",
-                           path, time_delta, points,
-                           (points * 1000.0 / time_delta)));
-    return points;
-  }
+		// get a config object
+		Config config = CliOptions.getConfig(argp);
 
-  /**
-   * Opens a file for reading, handling gzipped files.
-   * @param path The file to open.
-   * @return A buffered reader to read the file, decompressing it if needed.
-   * @throws IOException when shit happens.
-   */
-  private static BufferedReader open(final String path) throws IOException {
-    InputStream is = new FileInputStream(path);
-    if (path.endsWith(".gz")) {
-      is = new GZIPInputStream(is);
-    }
-    // I <3 Java's IO library.
-    return new BufferedReader(new InputStreamReader(is));
-  }
+		final TSDB tsdb = new TSDB(config);
+		tsdb.checkNecessaryTablesExist().joinUninterruptibly();
+		argp = null;
+		try {
+			int points = 0;
+			final long start_time = System.nanoTime();
+
+			for (final String path : args) {
+				points += importFile(tsdb.getClient(), tsdb, path);
+			}
+
+			final double time_delta = (System.nanoTime() - start_time) / 1000000000.0;
+			LOG.info(String.format("Total: imported %d data points in %.3fs"
+					+ " (%.1f points/s)",
+					points, time_delta, (points / time_delta)));
+		} finally {
+			try {
+				tsdb.shutdown().joinUninterruptibly();
+			} catch (Exception e) {
+				LOG.error("Unexpected exception", e);
+				System.exit(1);
+			}
+		}
+	}
+
+	private static int importFile(final HBaseClient client,
+			final TSDB tsdb,
+			final String path) throws IOException {
+
+		final long start_time = System.nanoTime();
+		long ping_start_time = start_time;
+		final BufferedReader in = open(path);
+		String line = null;
+		int points = 0;
+		try {
+			while ((line = in.readLine()) != null) {
+				final String[] words = Tags.splitString(line, ' ');
+				final String metric = words[0];
+				if (metric.length() <= 0) {
+					throw new RuntimeException("invalid metric: " + metric);
+				}
+				long timestamp = Tags.parseLong(words[1]);
+				if (timestamp <= 0) {
+					throw new RuntimeException("invalid timestamp: " + timestamp);
+				}
+
+				final String value = words[2];
+				if (value.length() <= 0) {
+					throw new RuntimeException("invalid value: " + value);
+				}
+				final HashMap<String, String> tags = new HashMap<String, String>();
+				for (int i = 3; i < words.length; i++) {
+					if (!words[i].isEmpty()) {
+						Tags.parse(tags, words[i]);
+					}
+				}
+
+				CachedBatches.addPoint(tsdb, metric, timestamp, value, tags);
+				points++;
+				if (points % 1000000 == 0) {
+					final long now = System.nanoTime();
+					ping_start_time = (now - ping_start_time) / 1000000;
+					LOG.info(String.format("... %d data points in %dms (%.1f points/s), freemem: %d",
+							points, ping_start_time,
+							(1000000 * 1000.0 / ping_start_time), 
+							Runtime.getRuntime().freeMemory()));
+					ping_start_time = now;
+				}
+			}
+		} catch (RuntimeException e) {
+			LOG.error("Exception caught while processing file "
+					+ path + " line=" + line);
+			throw e;
+		} finally {
+			in.close();
+		}
+		final long time_delta = (System.nanoTime() - start_time) / 1000000;
+		LOG.info(String.format("Processed %s in %d ms, %d data points"
+				+ " (%.1f points/s)",
+				path, time_delta, points,
+				(points * 1000.0 / time_delta)));
+		return points;
+	}
+
+	/**
+	 * Opens a file for reading, handling gzipped files.
+	 * @param path The file to open.
+	 * @return A buffered reader to read the file, decompressing it if needed.
+	 * @throws IOException when shit happens.
+	 */
+	private static BufferedReader open(final String path) throws IOException {
+		InputStream is = new FileInputStream(path);
+		if (path.endsWith(".gz")) {
+			is = new GZIPInputStream(is);
+		}
+		// I <3 Java's IO library.
+		return new BufferedReader(new InputStreamReader(is));
+	}
 
 }
