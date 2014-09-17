574ad1eabc668864141b9ce3676cf513143a9d74
haden dude <haden.dude@gmail.com>
2014-09-16 17:07:49 -0700

    testspeed can parse lines with ByteBuffer

--- src/tools/TestSpeed.java
+++ src/tools/TestSpeed.java
@@ -9,7 +9,6 @@ import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.charset.Charset;
-import java.util.zip.GZIPInputStream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,7 +18,7 @@ public class TestSpeed {
 	private static final Logger LOG = LoggerFactory.getLogger(TestSpeed.class);
 
 	private static void usage(final ArgP argp) {
-		System.err.println("Usage: testspeed [--bb] path");
+		System.err.println("Usage: testspeed [--bb=SIZE] [--buffer=SIZE] [--lines] path");
 		System.err.print(argp.usage());
 		System.exit(-1);
 	}
@@ -27,7 +26,7 @@ public class TestSpeed {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		ArgP argp = new ArgP();
-		argp.addOption("--bb", "SIZE", "use byte buffer. SIZE is the number of lines in the file");
+		argp.addOption("--bb", "use byte buffer. SIZE is the number of lines in the file");
 		argp.addOption("--buffer", "SIZE", "(default 64) size of ByteBuffer in Koctets");
 		
 		args = CliOptions.parse(argp, args);
@@ -39,7 +38,7 @@ public class TestSpeed {
 		final boolean useByteBuffer = argp.has("--bb");
 		
 		if (useByteBuffer) {
-			final int numLines = Integer.parseInt(argp.get("--bb"));
+			final int numLines = argp.get("--bb") != null ? Integer.parseInt(argp.get("--bb")) : 0;
 			final int bufferSize = Integer.parseInt(argp.get("--buffer", "64"));
 			readWithBB(path, numLines, bufferSize);
 		} else {
@@ -69,40 +68,45 @@ public class TestSpeed {
 		final FileChannel fc = fis.getChannel();
 		final ByteBuffer bb = ByteBuffer.allocate(1024*bufferSize);
 		Charset encoding = Charset.forName(System.getProperty("file.encoding"));
-/*
+
 		final char[] line = new char[1024]; 
 		int nextChar = 0;
 		boolean skipLF = false;
-*/
+		int points = 0;
+
 		final long start_time = System.nanoTime();
 
 		while (fc.read(bb) > 0) {
 			bb.flip();
 			
 			final CharBuffer cb = encoding.decode(bb);
-/*
-			for (int i = 0; i < cb.limit(); i++) {
-				char c = cb.get();
 
-				if (c == '\n' && skipLF) { // ignore this '\n'
-					skipLF = false;
-				} else if (c == '\n' || c == '\r') {
-					// handle line[0..nextChar[
-					nextChar = 0;
-					points++;
+			if (numLines == 0) {
+				for (int i = 0; i < cb.limit(); i++) {
+					char c = cb.get();
 
-					if (c == '\r') {
-						skipLF = true;
+					if (c == '\n' && skipLF) { // ignore this '\n'
+						skipLF = false;
+					} else if (c == '\n' || c == '\r') {
+						// handle line[0..nextChar[
+						nextChar = 0;
+						points++;
+
+						if (c == '\r') {
+							skipLF = true;
+						}
+					} else {
+						line[nextChar++] = c;
 					}
-				} else {
-					line[nextChar++] = c;
 				}
 			}
-*/
+
 			bb.clear();
 		}
 
-		displayAvgSpeed(start_time, numLines);
+		if (numLines > 0) points = numLines;
+		
+		displayAvgSpeed(start_time, points);
 
 		fc.close();
 		fis.close();
