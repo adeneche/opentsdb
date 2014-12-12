c7861c77a6414a749bf5430dcb4d46c8de3f83f1
adeneche <adeneche@gmail.com>
2014-12-11 20:56:31 -0800

    cleaned import2 from unnecessary byte conversion

--- src/tools/TextImporter2.java
+++ src/tools/TextImporter2.java
@@ -17,20 +17,15 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.zip.GZIPInputStream;
 
 import net.opentsdb.core.CachedBatches;
-import net.opentsdb.core.Const;
 import net.opentsdb.core.TSDB;
 import net.opentsdb.core.Tags;
 import net.opentsdb.utils.Config;
 
-import org.hbase.async.Bytes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -39,8 +34,6 @@ final class TextImporter2 {
 	private static final Logger LOG = LoggerFactory.getLogger(TextImporter2.class);
 	
 	private static boolean toScreen;
-
-	private static final List<MetricTags> metricTags = new ArrayList<MetricTags>();
 
 	/** Prints usage and exits. */
 	private static void usage(final ArgP argp) {
@@ -81,9 +74,6 @@ final class TextImporter2 {
 			for (final String path : args) {
 				LOG.info("Importing file {}", path);
 				points += importFile(tsdb, path);
-
-				// we don't need to share the metricTags between files
-				metricTags.clear();
 			}
 			
 			displayAvgSpeed(start_time, points);
@@ -122,9 +112,7 @@ final class TextImporter2 {
 		try {
 			while ((line = in.readLine()) != null) {
 				final String[] words = Tags.splitString(line, ' ');
-				final TimeValue dp = processLine(words);
-				
-				importDataPoint(tsdb, dp);
+				processAndImportLine(tsdb, words);
 
 				points++;
 				
@@ -142,7 +130,7 @@ final class TextImporter2 {
 		return points;
 	}
 
-	private static TimeValue processLine(final String[] words) {
+	private static void processAndImportLine(final TSDB tsdb, final String[] words) {
 		final String metric = words[0];
 		if (metric.length() <= 0) {
 			throw new RuntimeException("invalid metric: " + metric);
@@ -163,32 +151,12 @@ final class TextImporter2 {
 			Tags.parse(tags, words[i]);
 		}
 
-		final MetricTags mts = new MetricTags(metric, tags);
-
-		int metricTagsId = metricTags.indexOf(mts);
-		if (metricTagsId < 0) {
-			metricTagsId = metricTags.size();
-			metricTags.add(mts);
-		}
-
-		return new TimeValue(metricTagsId, timestamp, value);
-	}
-
-	/**
-	 * Import a datapoint into TSDB using CachedBatches. Applies concatenation+repetition offset and generates duplicates if necessary 
-	 */
-	private static void importDataPoint(final TSDB tsdb, TimeValue dp) {
-
-		final MetricTags mts = metricTags.get(dp.getMtsIndex());
-
-		final HashMap<String, String> tags = new HashMap<String, String>(mts.tags);
-
 		if (tsdb != null) {
-			CachedBatches.addPoint(tsdb, mts.metric, dp.timestamp, dp.getValueString(), tags);
+			CachedBatches.addPoint(tsdb, metric, timestamp, value, tags);
 		} 
 
 		if (toScreen) {
-			logDataPoint(mts.metric, dp.timestamp, dp.getValueString(), tags);
+			logDataPoint(metric, timestamp, value, tags);
 		}
 	}
 	
@@ -224,102 +192,6 @@ final class TextImporter2 {
 		}
 		// I <3 Java's IO library.
 		return new BufferedReader(new InputStreamReader(is));
-	}
-
-	/**
-	 * converts a timestamp to millis if it is in seconds
-	 * @return
-	 */
-	private static long toMillis(long t) {
-		if (isMillis(t)) return t;
-
-		return t * 1000;
-	}
-
-	private static boolean isMillis(long t) {
-		return (t & Const.SECOND_MASK) != 0;
-	}
-
-	private static class MetricTags {
-		public final String metric;
-		public final Map<String, String> tags;
-
-		public MetricTags(final String metric, final Map<String, String> tags) {
-			this.metric = metric;
-			this.tags = tags;
-		}
-
-		@Override
-		public int hashCode() {
-			return (metric + tags).hashCode();
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (obj == null || !(obj instanceof MetricTags))
-				return false;
-			MetricTags mts = (MetricTags)obj;
-			return metric.equals(mts.metric) && tags.equals(mts.tags);
-		}
-	}
-
-	private static class TimeValue { // (2+8+4) = 14 bytes
-		public static final int SIZE = 14; // in bytes
-		
-		private final short mtsIdx;
-		public final long timestamp;
-		private final int ivalue;
-
-		public boolean isFloat() {
-			return mtsIdx < 0;
-		}
-		
-		public String getValueString() {
-			if (isFloat())
-				return String.valueOf(Float.intBitsToFloat(ivalue));
-			else
-				return String.valueOf(ivalue);
-		}
-		
-		public int getMtsIndex() {
-			return mtsIdx;
-		}
-
-		public static TimeValue fromByteArray(final byte[] bytes, final int off) {
-			int cur = off * TimeValue.SIZE;
-			final short mtsIdx = Bytes.getShort(bytes, cur); cur+= 2;
-			final long timestamp = Bytes.getLong(bytes, cur); cur+= 8;
-			final int ivalue = Bytes.getInt(bytes, cur); cur+= 4;
-			
-			return new TimeValue(mtsIdx, timestamp, ivalue);
-		}
-
-		public static void toByteArray(final byte[] bytes, final int off, final TimeValue dp) {
-			int cur = off * TimeValue.SIZE;
-			System.arraycopy(Bytes.fromShort(dp.mtsIdx), 0, bytes, cur, 2); cur+= 2;
-			System.arraycopy(Bytes.fromLong(dp.timestamp), 0, bytes, cur, 8); cur+= 8;
-			System.arraycopy(Bytes.fromInt(dp.ivalue), 0, bytes, cur, 4); cur+= 4;
-		}
-		
-		public TimeValue(final int mtsIdx, final long timestamp, final String value) {
-			this.timestamp = timestamp;
-			
-			boolean isfloat = !Tags.looksLikeInteger(value);
-			if (isfloat) {
-				float fval = Float.parseFloat(value);
-				ivalue = Float.floatToRawIntBits(fval);
-				this.mtsIdx = (short) -mtsIdx;
-			} else {
-				ivalue = Integer.parseInt(value);
-				this.mtsIdx = (short) mtsIdx;
-			}
-		}
-
-		public TimeValue(final short mtsIdx, final long timestamp, final int ivalue) {
-			this.mtsIdx = mtsIdx;
-			this.timestamp = timestamp;
-			this.ivalue = ivalue;
-		}
 	}
 
 }
